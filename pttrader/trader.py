b96e31752ddcc015e5c5123e0d9bcb793999a27e
mike <mikhashev@yandex.ru>
2021-04-02 19:09:13 +0700

    Add trader module with three classes Add description for each class Add csv files as examples

--- pttrader/trader.py
+++ pttrader/trader.py
@@ -1,2 +1,31 @@
 
 
+class Wallet:
+    """
+    Class wallet that keep trader's money.
+    Contain currency type: "USD", "EUR", "RUR" etc. and amount
+    Variant 1: Wallet as two  unic .csv tables as database for Trader's account see:
+     wallet_current_state_example.csv
+     wallet_history_example.csv
+    """
+    pass
+
+class Account:
+    """
+    Class Account contain personal information about Traider:
+    Login
+    Password
+    ID
+    """
+    pass
+
+class Portfolio:
+    """
+    Class Portfolio contain info about Trader's assets
+    Variant 1: Portfolio as two  unic .csv tables as database for Trader's account see:
+     portfolio_current_state_example.csv Need to think about instrument price calculation if I bought 1 amount
+      of A ticker at price 100 and than 1 amount of A ticker at price 200 than price now will be?
+     
+     portfolio_history_example.csv
+    """
+    pass
