41ba59ed3c088ebcfa2798955fa6a7b2cccacd00
mike <mikhashev@yandex.ru>
2021-04-05 17:45:11 +0700

    Add two new modules: broker and market

---
+++ pttrader/market.py
@@ -0,0 +1,9 @@
+
+
+class Assets:
+    """
+    Class contain assets (stock, currency, bond and etc)  information from market and operation
+    """
+
+    # for example get info from tinkoff https://api-invest.tinkoff.ru/trading/stocks/get?ticker=UWGN
+    pass
