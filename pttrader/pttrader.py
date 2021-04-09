44c403a7d83cc8f736e11a1bc34fe75611d8fe0b
mike <mikhashev@yandex.ru>
2021-04-09 22:08:33 +0700

    Refactor from main to other modules

--- pttrader/pttrader.py
+++ pttrader/pttrader.py
@@ -1,171 +1,7 @@
-import datetime as dt
 import time
-from random import randint
-
 import login
 import trader
-import market
 import broker
-
-
-
-def trader_deposit_subtraction(currency, buy_order_price, amount):
-    """
-    This function subtract money from trader deposit by amount * buy_order_price BEFORE order complete at market
-    :param currency: currency of order USD, RUR, etc
-    :param buy_order_price:
-    :param amount:
-    :return: trader_deposit
-    """
-    global TRADER_DEPOSIT
-
-    TRADER_DEPOSIT[currency] -= buy_order_price * amount
-
-    print("Trader deposit changed: ", TRADER_DEPOSIT)
-
-
-def trader_deposit_addition(currency, sell_order_price, amount):
-    """
-    This function add money to TRADER_DEPOSIT by amount * buy_order_price AFTER order complete at market
-    :param currency: currency of order USD, RUR, etc
-    :param sell_order_price:
-    :param amount:
-    :return: trader_deposit
-    """
-    global TRADER_DEPOSIT
-
-    # Before money will add to TRADER_DEPOSIT, order status must be DONE or SOLD
-
-    TRADER_DEPOSIT[currency] += sell_order_price * amount
-
-    print("Trader deposit changed: ", TRADER_DEPOSIT)
-
-
-def order_buy_limit(operation_type="Buy", ticker="RSTK", buy_order_price=110.3, amount=1, currency="RUR",
-                    created_at=dt.datetime.utcnow()):
-    """
-    This function make order to buy amount of stocks by ticker name
-    specified at buy_order_price and wait until market price of stock ticker reach
-    buy_order_price
-    :param operation_type:
-    :param currency:
-    :param created_at:
-    :param ticker: ticker name
-    :param buy_order_price: price to buy ticker
-    :param amount: number of stocks to buy
-    :return: order_id
-    """
-
-    operation_type = order_data[0]
-    ticker = order_data[1]
-    buy_order_price = order_data[2]
-    amount = order_data[3]
-    currency = "RUR"
-    created_at = order_data[4]
-    buy_order_data = [operation_type, ticker, buy_order_price, amount, currency, created_at]
-    print("You place limit buy order for: ", ticker, ", amount lot is: ", amount, ", by price: ",
-          buy_order_price)
-    trader_deposit_subtraction(currency, buy_order_price, amount)
-    time.sleep(1)
-    return buy_order_data
-
-
-def order_sell_limit(ticker="UWGN", currency="RUR", sell_order_price=110.3, amount=1, created_at=dt.datetime.utcnow()):
-    """
-
-    This function make order to sell amount of stocks by ticker name
-    specified at sell_order_price and wait until market price of stock ticker reach
-    sell_order_price
-    :param currency:
-    :param created_at:
-    :param ticker: ticker name
-    :param sell_order_price: price to buy ticker
-    :param amount: number of stocks to buy
-    :return: order_id #
-    """
-
-    order_data = [ticker, sell_order_price, amount, created_at]
-
-    print("You place limit sell order ", ticker, "amount shares (!lots) is ", amount, "by price ",
-          sell_order_price)
-    trader_deposit_addition(currency, sell_order_price, amount)
-
-    return order_data
-
-
-def order_place_to_market(type_order_data):
-    """
-    Create order by individual id returned from buy_order_limit foo
-    :param type_order_data:
-    :param order_data:
-    :param order_id:
-    :return:
-    """
-    global NEW_ORDER_LIST
-
-    NEW_ORDER_LIST.append(type_order_data)
-    print("Order is placed", type_order_data)
-    print("return ", NEW_ORDER_LIST)
-
-
-
-
-
-def check_order_status(order):
-    """
-    Checking ticker market price and order_price is will be meet conditions to buy:
-    market_price is less or equal than order_price
-    condition to to sell:
-    market_price is greater or equal than order_price
-    :param order:
-    :return: True or False
-    """
-    operation_type = order[0]
-    ticker = order[1]
-    market_price = market.get_ticker_price(ticker)
-    order_price = order[2]
-    flag = False
-    # condition for buy order
-    print("Order status checking")
-    if operation_type == "Buy" and market_price <= order_price:
-        print("Market price <= Order price condition for Buy is True")
-        order_done_at = dt.datetime.utcnow()
-        flag = True
-        order_status = [flag, order_done_at]
-        return order_status
-
-    elif operation_type == "Sell" and market_price >= order_price:
-        print("Market price >= Order price condition for Sell is True")
-        order_done_at = dt.datetime.utcnow()
-        flag = True
-        order_status = [flag, order_done_at]
-        return order_status
-    else:
-        print("Wrong condition")
-        flag = False
-        order_done_at = dt.datetime.utcnow()
-        order_status = [flag, order_done_at]
-        return order_status
-
-
-def check_new_orders(new_order_list):
-    for order in new_order_list:
-        print(order)
-        order_status = check_order_status(order)
-        if order_status[0]:
-            print("Order ", order, "status is True", )
-
-            done_order = [order, order_status[1]]
-            TRADING_HISTORY_LIST.append(done_order)
-
-            NEW_ORDER_LIST.remove(order)
-            print(NEW_ORDER_LIST)
-
-        else:
-            print("Checking...")
-    if not NEW_ORDER_LIST:
-        print("There are no new orders ", NEW_ORDER_LIST)
-        print("Orders history here: ", TRADING_HISTORY_LIST)
 
 
 def market_manager(user_account_id):
@@ -230,21 +66,13 @@ def market_manager(user_account_id):
             # print("Sleep 5 sec")
 
 
-if __name__ == "__main__":
+def main():
     # starting program, waiting for User log in
     account_id = login.wait_logging()
-    print("Hello,",account_id)
-
+    print("Hello,", account_id)
     # main cycle
     market_manager(account_id)
 
-    # # check if account_id have portfolio -> show portfolio status
-    # current_trader_portfolio = trader.Portfolio()
-    # current_trader_portfolio.show()
-    # # check if account_id have wallet -> show wallet status
-    # current_trader_wallet = trader.Wallet()
-    # current_trader_wallet.show()
-    #
-    # # Wait for commands from trader
-    # # market.get_stock_data("UWGN")
-    # get_ticker_price()
+
+if __name__ == "__main__":
+    main()
